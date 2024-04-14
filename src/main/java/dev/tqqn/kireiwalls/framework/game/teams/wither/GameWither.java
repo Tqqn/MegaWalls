d05dc2c145d6b2ded5ceb0231a448ea4ff6c7c2b
Tqqn <mailtoonie@gmail.com>
2024-04-14 11:37:20 +0200

    commit

---
+++ src/main/java/dev/tqqn/kireiwalls/framework/game/teams/wither/GameWither.java
@@ -0,0 +1,13 @@
+package dev.tqqn.kireiwalls.framework.game.teams.wither;
+
+import org.bukkit.entity.Wither;
+
+public class GameWither {
+
+    private String name;
+    private Wither witherEntity;
+    private int health;
+
+
+
+}
