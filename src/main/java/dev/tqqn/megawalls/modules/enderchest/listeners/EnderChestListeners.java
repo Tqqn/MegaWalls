83c0715fa61fe1d50fc6af6649c8202a6c902868
Tqqn <mailtoonie@gmail.com>
2025-01-20 23:24:40 +0100

    [FIX] Kit items are not able to be stored in EnderChests or chests anymore.

--- src/main/java/dev/tqqn/megawalls/modules/enderchest/listeners/EnderChestListeners.java
+++ src/main/java/dev/tqqn/megawalls/modules/enderchest/listeners/EnderChestListeners.java
@@ -5,6 +5,7 @@ import dev.tqqn.megawalls.modules.enderchest.EnderChestModule;
 import dev.tqqn.megawalls.modules.game.GameModule;
 import dev.tqqn.megawalls.modules.game.framework.GameStates;
 import dev.tqqn.megawalls.modules.game.framework.events.GamePlayerKilledEvent;
+import dev.tqqn.megawalls.modules.menu.framework.Menu;
 import dev.tqqn.megawalls.modules.teams.framework.GameTeam;
 import dev.tqqn.megawalls.modules.teams.framework.wither.GameWither;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +16,38 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 @RequiredArgsConstructor
 public final class EnderChestListeners implements Listener {
 
     private final EnderChestModule enderChestModule;
     private final GameModule gameModule;
+
+    @EventHandler
+    public void onItemMoveOtherInv(InventoryClickEvent event) {
+        if (event.getInventory().getHolder() instanceof Menu) return;
+        if (event.getInventory().getType() != InventoryType.CHEST && event.getInventory().getType() != InventoryType.ENDER_CHEST) return;
+
+        ItemStack itemStack = (event.getClick() == ClickType.NUMBER_KEY ? event.getWhoClicked().getInventory().getItem(event.getHotbarButton()) : event.getCurrentItem());
+        if (itemStack == null) return;
+        if (itemStack.getType() == Material.AIR) return;
+
+
+        final ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null) return;
+
+        if (!itemMeta.hasLocalizedName()) return;
+        if (!itemMeta.getLocalizedName().equals("kit")) return;
+
+        event.setCancelled(true);
+    }
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
